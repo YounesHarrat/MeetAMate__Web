2bf3cd5f2f0859e28b7153fc2e2f67c93fac49dc
SeeRak <melenchon.guillaume@gmail.com>
2021-03-14 11:09:44 +0100

    Ajout components de base (squelette)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
 import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
 import { LeftSideNavBarComponent } from './components/left-side-nav-bar/left-side-nav-bar.component';
+import { CenteredContentComponent } from './components/centered-content/centered-content.component';
 import { RightSideNavBarComponent } from './components/right-side-nav-bar/right-side-nav-bar.component';
 import { BotNavBarComponent } from './components/bot-nav-bar/bot-nav-bar.component';
-import { CenteredContentComponent } from './components/centered-content/centered-content.component';
-import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
     LeftSideNavBarComponent,
+    CenteredContentComponent,
     RightSideNavBarComponent,
     BotNavBarComponent,
-    CenteredContentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/centered-content/centered-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centered-content',
+  templateUrl: './centered-content.component.html',
+  styleUrls: ['./centered-content.component.css']
+})
+export class CenteredContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
