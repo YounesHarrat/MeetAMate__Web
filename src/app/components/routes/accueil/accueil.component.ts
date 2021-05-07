df6436ed9a0df8a57eb7747ce2590527be37d846
Tournon Kilian <56253851+kikstrn@users.noreply.github.com>
2021-05-07 10:34:02 +0200

    Add V2 page accueil

--- src/app/components/routes/accueil/accueil.component.ts
+++ src/app/components/routes/accueil/accueil.component.ts
@@ -7,6 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
+  stockImg = [
+    {src:"assets/games/apex.jpg"},
+    {src:"assets/games/fortnite.jpg"},
+    {src:"assets/games/rocket.jpg"},
+    {src:"assets/games/osu.jpg"},
+    {src:"assets/games/lol.jpg"},
+    {src:"assets/games/Valorant.png"},
+    {src:"assets/games/overwatch.jpg"},
+    {src:"assets/games/minecraft.jpeg"},
+    {src:"assets/games/warzone.jpg"},
+    {src:"assets/games/csgo.jpg"},
+    {src:"assets/games/phasmophobia.jpg"},
+    {src:"assets/games/raft.jpg"},
+
+    
+  ];
+
+  plateformes = [
+    {src:"assets/plateformes/pc.png"},
+    {src:"assets/plateformes/play.png"},
+    {src:"assets/plateformes/Xbox.png"},
+    {src:"assets/plateformes/switch.png"},
+  ];
+
+  cardImg = [
+    {src:"assets/games/fortnite.jpg"},
+    {src:"assets/games/apex.jpg"},
+    {src:"assets/games/rocket.jpg"},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
