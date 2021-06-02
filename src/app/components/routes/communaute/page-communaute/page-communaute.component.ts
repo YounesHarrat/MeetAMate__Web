46b309f5fd90ccbdcbc332a108d044272da6b4c6
SeeRak <melenchon.guillaume@gmail.com>
2021-06-02 22:33:18 +0200

    vu design basique question

--- src/app/components/routes/communaute/page-communaute/page-communaute.component.ts
+++ src/app/components/routes/communaute/page-communaute/page-communaute.component.ts
@@ -22,13 +22,20 @@ export class PageCommunauteComponent implements OnInit {
     {id:"4", idJeu:"1", title:"Tournoi", type:"4v4", date:"28/04/2021", heure:"11h00"},
     {id:"5", idJeu:"1", title:"Solo", type:"1v1", date:"28/04/2021", heure:"11h00"},    
     ]
+  ListeQuestion = [
+    {id:"1", idJeu:"1", date:"28/04/2021", heure:"11h00", contenu:"Quel est le Dominus : Snake Skin ?"},
+    {id:"2", idJeu:"1", date:"28/04/2021", heure:"11h00", contenu:"Comment s'appelle la nouvelle mise à jour sortie le 4 octobre 2016 ?"},
+    {id:"3", idJeu:"1", date:"28/04/2021", heure:"11h00", contenu:"Qui est le joueur emblématique de Rocket League ?"},
+    {id:"4", idJeu:"1", date:"28/04/2021", heure:"11h00", contenu:"Quel est le véhicule qu'on a en tout premier ?"},
+    {id:"5", idJeu:"1", date:"28/04/2021", heure:"11h00", contenu:"Quel était l'ancêtre de Rocket League ?"},  
+  ]
     
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
 
   }
-
+  
   clickEvents(){
     this.btnEventClick = true
     this.btnQuestionClick = false;
