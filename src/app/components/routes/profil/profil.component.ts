c79a959d9435556f039c90fe45472555eaaab0b1
YounesHarrat <harratanis@hotmail.fr>
2021-08-10 10:07:22 +0200

    -m change of bdd structure , make:migration and migrate to build your bdd

--- src/app/components/routes/profil/profil.component.ts
+++ src/app/components/routes/profil/profil.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog/dialog';
+import { MatDialogRef } from '@angular/material/dialog/dialog-ref';
 import { AuthService } from '@auth0/auth0-angular';
+import { User } from 'src/app/models/user';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
@@ -11,9 +14,42 @@ export class ProfilComponent implements OnInit {
 
   constructor(public userService: UserService, public auth0: AuthService) { }
 
-
+  user = new User();
   ngOnInit(): void {
+    this.user = this.userService.user;
+    if ( this.user.pseudo ) {
 
+    } else {
+      console.log('%c SEEMS LIKE YOUR PROFILE ISNT FINISHED YET ', 'color:red');
+    }
+  }
+
+  openProfileModal() {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
