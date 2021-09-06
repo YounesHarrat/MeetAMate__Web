ff114e49b5aa05eafd08cd17c6dbd0d2995fb856
YounesHarrat <harratanis@hotmail.fr>
2021-09-06 09:24:05 +0200

    push work made for dialog user info

--- src/app/shared/user-profil-dialog/user-profil-dialog.component.ts
+++ src/app/shared/user-profil-dialog/user-profil-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-user-profil-dialog',
@@ -11,12 +13,19 @@ export class UserProfilDialogComponent implements OnInit {
   form!: any;
 
   constructor(
+    private userService: UserService,
     public dialogRef: MatDialogRef<UserProfilDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
 
   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      pseudo: new FormControl(),
+      nom: new FormControl(),
+      prenom: new FormControl(),
+      age: new FormControl(),
+   });
   }
 
   onNoClick(): void {
@@ -24,6 +33,15 @@ export class UserProfilDialogComponent implements OnInit {
   }
 
   closeDialog() {
+    this.dialogRef.close();
+  }
+
+  saveUser() {
+    console.log('checking form on save =>', this.form);
+    this.userService.setPseudo(this.form.value.pseudo);
+    this.userService.setPseudo(this.form.value.pseudo);
+
+    this.userService.updateUser();
     this.dialogRef.close();
   }
 }
