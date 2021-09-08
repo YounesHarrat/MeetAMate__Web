94ba90f5d4fa9567268dd7157851eb3289d6e9d3
SeeRak <melenchon.guillaume@gmail.com>
2021-09-08 16:30:11 +0200

    Remise des logo + pseudo(mock) sur page profil

--- src/app/components/routes/profil/pageProfil/main-page-profil/main-page-profil.component.ts
+++ src/app/components/routes/profil/pageProfil/main-page-profil/main-page-profil.component.ts
@@ -11,7 +11,7 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class MainPageProfilComponent implements OnInit {
 
-  // user = {
+  // user2 = {
   //   pseudo: "NachossOG",
   //   nom: "Harrat",
   //   prenom: "Younes",
